d6f2e4e95ca212ea457f2fab16bf45f3888329a6
Jeevika P <jeevikap2455@gmail.com>
2024-11-30 17:55:27 +0530

    Add files via upload

---
+++ for.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        for(int i=0;i<y;i++){
+            System.out.println(x);
+        }
+    }    
+}
