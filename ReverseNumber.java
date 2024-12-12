d89dad912021e0ff8ac6db8f20c415d4865952f3
Jeevika P <jeevikap2455@gmail.com>
2024-12-12 20:16:55 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int rev = 0;
+        while(n>0){
+            rev = rev * 10 + (n%10);
+            n = n/10;
+        }
+       System.out.println(rev);
+    }
+}
