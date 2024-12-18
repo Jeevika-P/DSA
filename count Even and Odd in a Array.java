0b43e63e4fff957361b1664d0f77ccee5548121b
Jeevika P <jeevikap2455@gmail.com>
2024-12-18 05:33:24 +0530

    Update count Even and Odd in a Array.java

--- count Even and Odd in a Array.java
+++ count Even and Odd in a Array.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
+class demo
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	    int n=5;
 	    int[] a = {1,2,3,4,5};
 		
-		int even = 0; // Initialize even counte
+		int even = 0; // Initialize even counter
 		int odd = 0;  // Initialize odd counter
 		for (int i = 0; i < n; i++) {
 		    if (a[i] % 2 == 0) { // Check if the number is even
